3800adb9f521600ea2f1c0fbc74300db778b74f1
AbigailChubb <psyac2@exmail.nottingham.ac.uk>
2018-10-30 11:32:15 +0000

    Update BinarySmile.m

--- BinarySmile.m
+++ BinarySmile.m
@@ -5,32 +5,36 @@ function [binarySmileNetwork ] = BinarySmile()
     load('labels.mat');
     %Invert labels in order to match the points (input) data.
     labels = labels';
-    k = 10; % k in k-cross validation
-
     
-    % Training loop
-    for i = 1:k
-            %Create a new binarySmileNetwork with 10 hidden nodes.
+    predicted = [];
+    actual = [];
+    
+    k = 10; % k in k-cross validation
+    
+    cv = cvpartition(size(points, 2), 'kfold', k);
+    for i = 1:cv.NumTestSets
+        % Training loop
+        %Create a new binarySmileNetwork with 10 hidden nodes.
         binarySmileNetwork = newff(points, labels, 10);
     
         binarySmileNetwork.name = 'BinarySmile';
         binarySmileNetwork.trainParam.epochs = 100;
         binarySmileNetwork.trainParam.show = 7;
-        binarySmileNetwork.trainParam.goal = 0;
-    
-%     k = 10;
-%     binarySmileNetwork = train(binarySmileNetwork, points, labels);
-
-    % Training setup
-
-        s = size(points, 2); % size of input set
-        testInd = round(s*0.9, 0):s; % Last 10% is test set
-        binarySmileNetwork.divideParam.testInd = testInd;
-        s = round(s*0.9, 0) - 1; % Decrease size of set as we don't want to use test set in training / validation
         
-        validInd = ((i-1)*(round(s/k,0)))+1:i*(round(s/k,0)); % Indicies in validation set, all else in train set
-        binarySmileNetwork.divideParam.valInd = validInd;
-        binarySmileNetwork.divideParam.trainInd = setdiff(1:s, validInd); % Larger set on left of setdiff
-        binarySmileNetwork = train(binarySmileNetwork, points, labels);
+        % Training setup
+
+        trainX = points(:, cv.training(i));
+        trainY = labels(cv.training(i));
+        testX = points(:, cv.test(i));
+        testY = labels(cv.test(i));
+
+        %train(net, inputs, taregts)
+        binarySmileNetwork = train(binarySmileNetwork, trainX, trainY);
+        t = sim(binarySmileNetwork, testX);
+        predicted = [predicted t];
+        actual = [actual testY];
+        
+        [c, cm] = confusion(testY, t);
+
     end
 end

53d9f8cbefde2a3c6de7e28e5587658ee0d8a5c4
Tingwei Zhou <psytz2@nottingham.ac.uk>
2018-11-08 13:01:29 +0000

    optimize the efficient of getEntropy and getGain when implement binary classification

--- Coursework 2/ChooseAttribute.m
+++ Coursework 2/ChooseAttribute.m
@@ -1,15 +1,15 @@
 
-% clear;
-% load('facialPoints.mat');
-% load('labels.mat');
-% features = reshape(points, [150,132]);
-% targets = labels;
+clear;
+load('facialPoints.mat');
+load('labels.mat');
+features = reshape(points, [150,132]);
+targets = labels;
 
 
 % This function aims to find the maximum gain from all possible attributes
 % Output: 1. bestFeature = the attribute index corresponding maximum GAIN
 %         2. bestThreshold =  the attribute value corresponding maximum GAIN
-function [bestFeature, bestThreshold] = ChooseAttribute(features, targets)
+% function [bestFeature, bestThreshold] = ChooseAttribute(features, targets)
      
      [rows, cols] = size(features);
      bestGain = -inf;
@@ -28,53 +28,58 @@ function [bestFeature, bestThreshold] = ChooseAttribute(features, targets)
              end
          end
      end
-     disp(bestFeature);
-     disp(bestThreshold);
-end
+     disp("best gain: "+bestGain);
+     disp("bestFeature: "+bestFeature);
+     disp("bestThreshold: "+bestThreshold);
+% end
 
 function entropy = getEntropy(targets)
-    categoryOfTarget = categorical(targets); %tranfer double type to categorical type
-    categoryCounts = countcats(categoryOfTarget); %obtain each category's count
-    totalNum = sum(categoryCounts); %get the total number of target 
+%------ General method --------
+%     count = hist(targets,unique(targets));
+%     entropy = 0;
+%     for i = 1:length(count)
+%         ratio = count(i)/totalNums;
+%         entropy = entropy - ratio * log2(ratio);
+%     end
+
+
+%------- Binary method(efficient than general method) --------
+    totalNums = length(targets);
     
+    zerosRadio = length(find(targets == 0))/totalNums;
+    onesRadio = length(find(targets == 1))/totalNums;
     entropy = 0;
-    for i = 1:length(categoryCounts)
-        posRetio = categoryCounts(i)/totalNum;
-        entropy = entropy - posRetio * log2(posRetio);
+    if zerosRadio ~= 0
+        entropy = entropy - zerosRadio*log2(zerosRadio);
     end
+    if onesRadio ~= 0
+        entropy = entropy - onesRadio*log2(onesRadio);
+    end
+
+
 end
 
 function gain = getGain(attribute, targets, threshold)
     entropy = getEntropy(targets);
-     
-    %if less than threshold, save current infor into lessT 
-    %if greater or equal than threshold, save current infor into
-    %greatOrEqual 
-    lessT = [];
-    greatOrEqualT = [];
-    for i = 1:size(targets,1)
-        if attribute(i) < threshold
-%             rows = size(lessT, 1);
-            lessT = [lessT; i attribute(i) targets(i)];
-        else
-%             rows = size(greatOrEqualT, 1);
-            greatOrEqualT = [greatOrEqualT; i attribute(i) targets(i)];
-        end
-    end
     
-    [labelRows, ~] = size(targets);
+    [sortAttribute, preIndex] = sort(attribute);
+    sortTargets = targets(preIndex, :);
+    thresholdIndex = find(sortAttribute == threshold);
+    
+    totalNums = size(targets, 1);
+    lNums = thresholdIndex - 1;
+    geNums = totalNums - lNums;
+    
+    lTargets = [];
+    geTargets = [];
+    if lNums ~= 0
+        lTargets = sortTargets(1:lNums);
+    end
+    if geNums ~= 0
+        geTargets = sortTargets(lNums+1:end);
+    end   
         
-    LTRows = size(lessT,1); 
-    GERows = size(greatOrEqualT,1);
-       
-    remainder = 0;
-    if LTRows ~= 0
-        remainder = remainder + LTRows/labelRows * getEntropy(lessT(:,3));
-    end
-    if GERows ~= 0
-        remainder = remainder + GERows/labelRows * getEntropy(greatOrEqualT(:,3));
-    end
-%     remainder = LTRows/labelRows * getEntropy(lessT(:,3)) + GERows/labelRows * getEntropy(greatOrEqualT(:,3));   
+    remainder = lNums/totalNums * getEntropy(lTargets) + geNums/totalNums * getEntropy(geTargets);   
     gain = entropy  - remainder;
   
 end

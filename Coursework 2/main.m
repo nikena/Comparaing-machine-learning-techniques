20fd3500dff18c5cd5f34a21adef70a3fd2b3135
Ru <psyrc5@exmail.nottingham.ac.uk>
2018-11-12 18:52:58 +0000

    Update main.m

--- Coursework 2/main.m
+++ Coursework 2/main.m
@@ -2,5 +2,5 @@
     load('facialPoints');
     points = reshape(points, [132, 150]);
     tree = DecisionTreeLearning(points,  labels);
-    DrawDecisionTree(tree);
-    kfoldresults = kfold(points, labels);
+    %DrawDecisionTree(tree);
+    f1score = kfold(points, labels);

b751f392771163220edd315cd44abef5f0c6920d
Ru <psyrc5@exmail.nottingham.ac.uk>
2018-11-12 18:55:49 +0000

    Final kfold

--- Coursework 2/kfold.m
+++ Coursework 2/kfold.m
@@ -1,46 +1,50 @@
 function kfolddecisiontree = kfold(points, labels) 
 
     k = 10;
+    
+    %splitting data into 10 folds
     [trainIdxGroups, testIdxGroups, trainSize, testSize] = KFoldSplitData(size(points, 2), k);
     
             
     predicted = [];
-    actual = [];
     
     for i = 1:k
     
         trainIdx = trainIdxGroups(:, i); 
         testIdx = testIdxGroups(:, i);
         
+        %training data 90%
         trainX = points(:, trainIdx);
         trainY = labels(trainIdx);
+        
+        %test data 10%
         testX = points(:, testIdx);
         testY = labels(testIdx);
-        
-        testY = testY';
-        actual = [actual testY];
-    
+       
         %call decisiontree on 90% of the data to build the tree
         tree = DecisionTreeLearning(trainX,  trainY);
-    
+        %DrawDecisionTree(tree);
         
-        %a temp tree to go through it and check the label
-    
+        %a temporary tree needed to go through it and check the label
+        %classifier
         for n = 1:size(testX, 2)
 
             temptree = tree;
-
+            %if no kids then leaf node
             while ~isempty(temptree.kids)
+              
                 if testX(temptree.attribute, n) < temptree.threshold
                     temptree = temptree.kids{1};
 
                 else
                     temptree = temptree.kids{2};
                 end
+
             end
         
         predicted = [predicted temptree.class];
+        kfolddecisiontree = f1score(predicted, testY);
         end
+        
     end
-    kfolddecisiontree = [predicted; actual];
 end

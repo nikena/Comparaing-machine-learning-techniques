0138652203364db41b180d0b8389524801a1eee5
Tingwei Zhou <psytz2@nottingham.ac.uk>
2018-11-13 10:26:46 +0000

    final structure of kfold

--- Coursework 2/kfold.m
+++ Coursework 2/kfold.m
@@ -22,52 +22,65 @@ function [kFoldRecall, kFoldPrecision, kFoldF1score, kFoldAccuracy] = kfold(poin
         testY = labels(testIdx);
       
         kFoldActual(i,:) = testY;
-        %call decisiontree on 90% of the data to build the tree
+        % call decisiontree on 90% of the data to build the tree
         tree = DecisionTreeLearning(trainX,  trainY);
-        
-        %a temp tree to go through it and check the label
-        for n = 1:testSize(i)
-
-            temptree = tree;
-
-            while ~isempty(temptree.kids)
-                if testX(n, temptree.attribute) < temptree.threshold
-                    temptree = temptree.kids{1};
-                else
-                    temptree = temptree.kids{2};
-                end
-            end
-        kFoldPredicted(i,n) = temptree.class;
-        end
-        
-%         plotconfusion(kFoldActual(i,:), kFoldPredicted(i,:));
-        TP = 0;
-        FP = 0;
-        TN = 0;
-        FN = 0;
-        for j = 1:testSize(i)
-            if kFoldActual(i,j) == kFoldPredicted(i,j)
-                if kFoldActual(i,j) == 1
-                    TP = TP + 1;
-                else
-                    TN = TN + 1;
-                end
-            elseif kFoldPredicted(i,j) == 1
-                FP = FP + 1;
-            else
-                FN = FN + 1;              
-            end            
-        end
-        kFoldAccuracy(i) = (TP + TN) / testSize(i);
-        kFoldRecall(i) = TP/(TP + FN);
-        kFoldPrecision(i) = TP/(TP + FP);
-        kFoldF1score(i) = fscore(1, kFoldRecall(i), kFoldPrecision(i));
+        % the input sample and output the predicted outcome of the tree
+        kFoldPredicted(i,:) = EvaluateTree(tree, testX);
+        % calculate the performance
+        [accuracy, recall, precision, f1score] = calPerformence(kFoldPredicted(i,:), testY);
+        kFoldAccuracy(i) = accuracy;
+        kFoldRecall(i) = recall;
+        kFoldPrecision(i) = precision;
+        kFoldF1score(i) = f1score;
     end
 end
+
 
 function fs = fscore(beta, recall, precision)
     fs = (1+(beta*beta))*((precision*recall)/((beta*beta*precision)+recall));
 end
 
+function outputs  = EvaluateTree(tree, inputs)
+    num = size(inputs,1);
+    outputs = zeros(1, num);
+    %a temp tree to go through it and check the label
+    for n = 1:num
+        temptree = tree;
+
+        while ~isempty(temptree.kids)
+            if inputs(n, temptree.attribute) < temptree.threshold
+                temptree = temptree.kids{1};
+            else
+                temptree = temptree.kids{2};
+            end
+        end
+    outputs(n) = temptree.class;
+    end
+end
+
+function [accuracy, recall, precision, f1score] = calPerformence(predicted, actual)
+    TP = 0;
+    FP = 0;
+    TN = 0;
+    FN = 0;
+    num = length(predicted);
+    for j = 1:num
+        if actual(j) == predicted(j)
+            if actual(j) == 1
+                TP = TP + 1;
+            else
+                TN = TN + 1;
+            end
+        elseif predicted(j) == 1
+            FP = FP + 1;
+        else
+            FN = FN + 1;              
+        end            
+    end
+    accuracy = (TP + TN) / num;
+   	recall = TP/(TP + FN);
+    precision = TP/(TP + FP);
+    f1score = fscore(1, recall, precision);
+end
 
 
